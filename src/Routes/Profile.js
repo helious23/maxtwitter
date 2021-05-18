030d2c518c347651d3054a8a30473cb94d190d3c
Max16 <max16@naver.com>
2021-05-18 09:46:12 +0900

    #2.6 Log Out

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,5 +1,18 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClock = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClock}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../Routes/Auth";
-import Home from "../Routes/Home";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import Auth from "Routes/Auth";
+import Home from "Routes/Home";
+import Profile from "Routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            <Redirect from="*" to="/" />
           </>
         ) : (
-          <Route exact path="/" component={Auth} />
+          <>
+            <Route exact path="/" component={Auth} />
+            <Redirect from="*" to="/" />
+          </>
         )}
       </Switch>
     </Router>
