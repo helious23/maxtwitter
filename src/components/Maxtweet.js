adb33f13090511ca51b37792e67b066ab8394a5e
Max16 <max16@naver.com>
2021-05-18 16:26:26 +0900

    #3.5 Delete and Update part Two

--- src/components/Maxtweet.js
+++ src/components/Maxtweet.js
@@ -1,13 +1,54 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
 const MaxTweet = ({ maxtweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newMaxtweet, setNewMaxtweet] = useState(maxtweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this one?");
+    if (ok) {
+      await dbService.doc(`maxtweets/${maxtweetObj.id}`).delete();
+    }
+  };
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`maxtweets/${maxtweetObj.id}`).update({
+      text: newMaxtweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewMaxtweet(value);
+  };
   return (
     <div>
-      <h4>{maxtweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete</button>
-          <button>Edit</button>
+          <form onSubmit={onSubmit}>
+            <input
+              onChange={onChange}
+              type="text"
+              placeholder="Edit your Maxtweet"
+              value={newMaxtweet}
+              required
+            />
+            <input type="submit" value="Update Maxtweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{maxtweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete</button>
+              <button onClick={toggleEditing}>Edit</button>
+            </>
+          )}
         </>
       )}
     </div>
