41d0e743b2ce8de6b1dd4deab82a22f0458e5a72
Max16 <max16@naver.com>
2021-05-19 19:40:28 +0900

    #5.1 Update Profile

--- src/Routes/Profile.js
+++ src/Routes/Profile.js
@@ -1,12 +1,27 @@
 import { authService, dbService } from "fbase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClock = () => {
     authService.signOut();
     history.push("/");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
   };
   const getMyMaxtweet = async () => {
     const maxtweets = await dbService
@@ -21,6 +36,15 @@ const Profile = ({ userObj }) => {
   }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClock}>Log Out</button>
     </>
   );
